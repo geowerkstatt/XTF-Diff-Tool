6680561c3cbb512c3c84d8c80829579cf12407d9
Dominic Burger <dominic.burger@geowerkstatt.ch>
2026-01-13 14:20:03 +0100

    Configure jar manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
     id("checkstyle")
 }
 
@@ -13,6 +14,18 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "ch.geowerkstatt.xtfdifftool.Main"
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = application.mainClass
+        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(" ") { file -> file.name }
+        attributes["Implementation-Version"] = version
+    }
 }
 
 tasks.test {
