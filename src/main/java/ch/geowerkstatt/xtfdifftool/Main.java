5ef7acee75d595b715eddb22e3b72a35f8622bca
Dominic Burger <dominic.burger@geowerkstatt.ch>
2026-01-13 13:53:42 +0100

    Configure checkstyle

--- src/main/java/ch/geowerkstatt/xtfdifftool/Main.java
+++ src/main/java/ch/geowerkstatt/xtfdifftool/Main.java
@@ -1,7 +1,13 @@
 package ch.geowerkstatt.xtfdifftool;
 
-public class Main {
-    public static void main(String[] args) {
+public final class Main {
+    private Main() { }
+
+    /**
+     * Application entry point.
+     * @param args Command line arguments.
+     */
+    static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
