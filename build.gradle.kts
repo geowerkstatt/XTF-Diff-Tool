f078e36e05eedfe0a0c4dcff7048db7d0a068dee
Dominic Burger <dominic.burger@geowerkstatt.ch>
2026-01-14 16:39:55 +0100

    Add xtf stream reader

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,13 @@ group = "ch.geowerkstatt.xtfdifftool"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jars.interlis.ch") }
 }
 
 dependencies {
+    implementation("ch.interlis:iox-ili:1.24.4")
+    implementation("ch.interlis:ili2c-core:5.6.8")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
