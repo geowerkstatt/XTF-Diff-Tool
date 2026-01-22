1b02b0ea144ca0877b8a15c8709c6cac2ba637d6
Dominic Burger <dominic.burger@geowerkstatt.ch>
2026-01-22 14:53:54 +0100

    Add validator for stable oids

---
+++ src/main/java/ch/geowerkstatt/xtfdifftool/ModelValidator.java
@@ -0,0 +1,48 @@
+package ch.geowerkstatt.xtfdifftool;
+
+import ch.interlis.ili2c.metamodel.AbstractClassDef;
+import ch.interlis.ili2c.metamodel.Domain;
+import ch.interlis.ili2c.metamodel.Element;
+import ch.interlis.ili2c.metamodel.TransferDescription;
+import ch.interlis.iom.IomObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class ModelValidator {
+    private final TransferDescription transfer;
+    private final Map<String, Boolean> validatedClasses = new HashMap<>();
+
+    /**
+     * Creates a new ModelValidator for the given transfer description.
+     * @param transfer The INTERLIS transfer description.
+     */
+    public ModelValidator(TransferDescription transfer) {
+        this.transfer = transfer;
+    }
+
+    /**
+     * Validates that the INTERLIS class of the given IomObject has a stable OID.
+     * @param iomObject The IomObject to validate.
+     * @return True if the class has a stable OID.
+     */
+    public boolean validateObjectHasStableOid(IomObject iomObject) {
+        return validatedClasses.computeIfAbsent(iomObject.getobjecttag(), this::validateClassHasStableOid);
+    }
+
+    private boolean validateClassHasStableOid(String className) {
+        Element classElement = transfer.getElement(className);
+        if (!(classElement instanceof AbstractClassDef<?> classDef)) {
+            System.err.println("Error: Class or Association \"" + className + "\" not found.");
+            return false;
+        }
+
+        Domain oid = classDef.getOid();
+        if (oid == null) {
+            System.err.println("Warning: Class or Association \"" + className + "\" has no stable OID.");
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/ch/geowerkstatt/xtfdifftool/ModelValidatorTest.java
@@ -0,0 +1,78 @@
+package ch.geowerkstatt.xtfdifftool;
+
+import ch.interlis.ili2c.config.Configuration;
+import ch.interlis.ili2c.config.FileEntry;
+import ch.interlis.ili2c.config.FileEntryKind;
+import ch.interlis.ili2c.metamodel.TransferDescription;
+import ch.interlis.iom.IomObject;
+import ch.interlis.iom_j.Iom_jObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public final class ModelValidatorTest {
+    private static final String MODEL_FILE = "src/test/data/ModelValidatorTest/Model.ili";
+    private ModelValidator validator;
+
+    @BeforeEach
+    public void setUp() {
+        Configuration config = new Configuration();
+        config.addFileEntry(new FileEntry(MODEL_FILE, FileEntryKind.ILIMODELFILE));
+        TransferDescription transfer = ch.interlis.ili2c.Main.runCompiler(config);
+        validator = new ModelValidator(transfer);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Model.TopicMissingOid.ClassOid",
+            "Model.TopicMissingOid.AssocOid",
+            "Model.TopicOid.ClassOid",
+    })
+    public void validateClassWithOid(String className) {
+        assertTrue(validateObjectHasStableOid(className));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Model.TopicOid.ClassMissingOid",
+    })
+    public void validateClassInTopicWithOid(String className) {
+        assertTrue(validateObjectHasStableOid(className));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Model.TopicMissingOid.ClassMissingOid",
+            "Model.TopicMissingOid.ClassNoOid",
+            "Model.TopicOid.ClassNoOid",
+            "Model.TopicOid.AssocMissingOid",
+    })
+    public void validateClassWithoutOid(String className) {
+        assertFalse(validateObjectHasStableOid(className));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Model.TopicOidExtended.ClassMissingOidExtended",
+            "Model.TopicOidExtended.ClassOidExtended",
+    })
+    public void validateExtendedClassWithOid(String className) {
+        assertTrue(validateObjectHasStableOid(className));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Model.TopicOidExtended.ClassNoOidExtended",
+    })
+    public void validateExtendedClassWithoutOid(String className) {
+        assertFalse(validateObjectHasStableOid(className));
+    }
+
+    private boolean validateObjectHasStableOid(String className) {
+        IomObject object = new Iom_jObject(className, "o1");
+        return validator.validateObjectHasStableOid(object);
+    }
+}
