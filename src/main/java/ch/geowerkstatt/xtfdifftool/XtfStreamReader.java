3549c201a19df5ded35a4e1dcc44e1b2b565063a
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2026-01-26 10:27:10 +0100

    Fix XtfStreamReader

--- src/main/java/ch/geowerkstatt/xtfdifftool/XtfStreamReader.java
+++ src/main/java/ch/geowerkstatt/xtfdifftool/XtfStreamReader.java
@@ -1,12 +1,14 @@
 package ch.geowerkstatt.xtfdifftool;
 
 import ch.ehi.basics.settings.Settings;
+import ch.interlis.ili2c.metamodel.TransferDescription;
 import ch.interlis.iom.IomObject;
 import ch.interlis.iox.EndTransferEvent;
 import ch.interlis.iox.IoxEvent;
 import ch.interlis.iox.IoxException;
 import ch.interlis.iox.IoxReader;
 import ch.interlis.iox.ObjectEvent;
+import ch.interlis.iox_j.IoxIliReader;
 import ch.interlis.iox_j.logging.LogEventFactory;
 import ch.interlis.iox_j.utility.ReaderFactory;
 
@@ -30,10 +32,13 @@ public final class XtfStreamReader implements AutoCloseable {
      * @param xtfFile The file to read from.
      * @throws IoxException If an error occurs while creating the transfer file reader.
      */
-    public XtfStreamReader(File xtfFile) throws IoxException {
+    public XtfStreamReader(TransferDescription transferDescription, File xtfFile) throws IoxException {
         LogEventFactory logEventFactory = new LogEventFactory();
         Settings settings = new Settings();
         this.reader = READER_FACTORY.createReader(xtfFile, logEventFactory, settings);
+        if (this.reader instanceof IoxIliReader iliReader) {
+            iliReader.setModel(transferDescription);
+        }
     }
 
     /**
